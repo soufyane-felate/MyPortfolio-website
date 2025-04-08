06914defda0a451fa6edc48fcecaf2d5d9fb89e0
soufyane-felate <soufyanfellat03@gmail.com>
2025-04-08 17:55:30 +0100

    Home, About and Navigation Bar pages created

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,19 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { SkillsComponent } from './skills/skills.component';
+import { HttpErrorResponse } from '@angular/common/http';
+import { ExperienceComponent } from './experience/experience.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { FirstBodyComponent } from './first-body/first-body.component';
+import { AboutComponent } from './about/about.component';
+import { FooterComponent } from "./footer/footer.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, SkillsComponent,
+    ExperienceComponent, NavbarComponent, FirstBodyComponent,
+    AboutComponent, FooterComponent,FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
---
+++ src/app/first-body/first-body.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-first-body',
+  standalone: true,
+  imports: [],
+  templateUrl: './first-body.component.html',
+  styleUrl: './first-body.component.css'
+})
+export class FirstBodyComponent {
+
+}
