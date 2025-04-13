b4c136fbc125c040b8f9e987308e8c347236afef
soufyane-felate <soufyanfellat03@gmail.com>
2025-04-14 00:08:35 +0100

    css fixed

--- src/app/projects/projects.component.ts
+++ src/app/projects/projects.component.ts
@@ -19,7 +19,7 @@ export class ProjectsComponent {
       UrlLink: 'https://github.com/soufyane-felate/BuildFlow-Manager',
       langue: ['java', 'html', 'css', 'jee', 'js'],
       Image: [
-        '../../assets/images/build.jpg',
+        '../../assets/images/build.',
         '../../assets/images/buildFlow.png',
         '../../assets/images/buildFlow2.png',
         '../../assets/images/buildFlow3.png',
@@ -35,18 +35,29 @@ export class ProjectsComponent {
         '../../assets/images/cookspot2.png',
       ],
     },
-   
     {
-      title: 'cookspot',
-      description: 'CookSpot is a user-friendly recipe website...',
-      UrlLink: 'https://github.com/soufyane-felate/BuildFlow-Manager',
+      title: 'movies',
+      description: 'A movie website that offers users the latest films, trailers, reviews, and detailed information about upcoming releases, all in one place.',
+      UrlLink: 'https://github.com/soufyane-felate/Movies',
       langue: ['js', 'html', 'css'],
       Image: [
         '../../assets/images/movies.png',
         '../../assets/images/movies2.png',
       ],
     },
+    {
+      title: 'weather',
+      description: 'Stay updated with real-time weather forecasts, temperature, and alerts for your location.',
+      UrlLink: 'https://github.com/soufyane-felate/weather',
+      langue: ['js', 'html', 'css'],
+      Image: [
+        '../../assets/images/w.png',
+        '../../assets/images/w1.png',
+        '../../assets/images/w3.png',
+      ],
+    },
    
+    
   ];
 
   getLanguages(): string[] {
